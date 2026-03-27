3f24b59a0a1fe59f128ac57e470f3c5e4b8556f7
Jonathan Helga <jonathanhelgalie@gmail.com>
2026-03-27 20:46:54 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,14 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
-  // TODO: Uncomment the code below to pass the first stage
-  // std::cout << "$ ";
-  // std::cout << "$ ";
-
-
-  // std::string input;
-  // std::getline(std::cin, input);
-  // std::cout << input << ": command not found" << std::endl;
-
-  while(1){
+  // std:bool loop = true;
+  while(true){
     std::cout << "$ ";
     std::string input;
     std::getline(std::cin, input);
+    if(input == "exit"){
+      return 0;
+    }
     std::cout << input << ": command not found" << std::endl;
   }
 }
