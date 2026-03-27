4a6debacae2b59692e3af3b037af50b1e8aa9b58
Jonathan Helga <jonathanhelgalie@gmail.com>
2026-03-27 23:04:29 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@
 #include <string>
 #include <set>
 #include <vector>
-
+// g++ -std=c++17 -o shell src/main.cpp
+//./shell
 namespace fs = std::filesystem;
 
 int main() {
@@ -23,14 +24,8 @@ int main() {
       std::string tempo = input.substr(5);
       if(commands.find(tempo) != commands.end()){ std::cout << tempo << " is a shell builtin" << std::endl; }
       else{ 
-        // std::cout << tempo << ": not found" << std::endl; 
-        char *path_env = std::getenv("PATH");
-        bool found = false;
+        std::cout << tempo << ": not found" << std::endl; 
 
-        if(path_env == nullptr){
-          std::string path_str = path_env;
-          std::cout << path_str << std::endl ;
-        }
       }
     }
     else if(input.substr(0, 5) == "echo ") {  std::cout << input.substr(5) << std::endl; }
