aceea54655a53c0d7f4bfa219a35eaa3d6b333a2
Jonathan Helga <jonathanhelgalie@gmail.com>
2026-03-27 21:47:08 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,8 @@ int main() {
     else if(input.substr(0, 5) == "type "){
       std::string tempo = input.substr(5);
       auto it = std::find(std::begin(commands), std::end(commands), tempo);
-      if(it != std::end(commands)){ std::cout << tempo << " is a shell builtin"; }
-      else{ std::cout << tempo << ": not found"; }
+      if(it != std::end(commands)){ std::cout << tempo << " is a shell builtin" << std::endl; }
+      else{ std::cout << tempo << ": not found" << std::endl; }
     }
     else if(input.substr(0, 5) == "echo ") {  std::cout << input.substr(5) << std::endl; }
     else { std::cout << input << ": command not found" << std::endl; }
