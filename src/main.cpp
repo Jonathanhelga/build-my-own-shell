0c24135fb3646de83571c0fc6faa8eb92f168793
Jonathan Helga <jonathanhelgalie@gmail.com>
2026-03-27 22:49:00 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
+#include <cstdlib>
+#include <filesystem>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <set>
+#include <vector>
+
+namespace fs = std::filesystem;
+
 int main() {
-  // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
@@ -16,16 +22,18 @@ int main() {
     else if(input.substr(0, 5) == "type "){
       std::string tempo = input.substr(5);
       if(commands.find(tempo) != commands.end()){ std::cout << tempo << " is a shell builtin" << std::endl; }
-      else{ std::cout << tempo << ": not found" << std::endl; }
+      else{ 
+        // std::cout << tempo << ": not found" << std::endl; 
+        char *path_env = std::getenv("PATH");
+        bool found = false;
+
+        if(path_env == nullptr){
+          std::string path_str = path_env;
+          std::cout << path_str << std::endl ;
+        }
+      }
     }
     else if(input.substr(0, 5) == "echo ") {  std::cout << input.substr(5) << std::endl; }
     else { std::cout << input << ": command not found" << std::endl; }
   }
 }
-
-// $ type echo
-// echo is a shell builtin
-// $ type exit
-// exit is a shell builtin
-// $ type invalid_command
-// invalid_command: not found
