a711d1c10a5c95afd5fd5f9233b6dbb39740e0d5
Jonathan Helga <jonathanhelgalie@gmail.com>
2026-03-28 13:03:15 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -80,7 +80,7 @@ int main(){
                         }
                     }
                 }else{ std::cout << program_name << ": not found" << std::endl; }
-
+                
             }else{ std::cout << program_name << ": not found" << std::endl; }
 
         }
