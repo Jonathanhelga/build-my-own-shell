6abb389332511941a739f0d9e4a1833a98ff6249
Jonathan Helga <jonathanhelgalie@gmail.com>
2026-03-29 19:14:41 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -52,20 +52,19 @@ std::vector <std::string> tokenize(const std::string &input){
       i++;
       if(input[i] == '\\') {current += input[i++]; }
       current += input[i];
-      // i++;
+      i++;
     }
     else if (c == ' ' || c == '\t'){
       if(!current.empty()){
         tokens.push_back(current);
         current.clear();
       }
-      // i++;
+      i++;
     }
     else{
       current += c;
-      // i++;
+      i++;
     }
-    i++;
   }
   if(!current.empty()){ tokens.push_back(current); }
   return tokens;
